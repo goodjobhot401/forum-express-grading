bf21c1bd61cfe95dee0429ff437b62faf7fad978
goodjobhot401 <goodjobhot@gmail.com>
2023-05-30 10:47:18 +0800

    feat: add restaurant-services

--- controllers/apis/restaurant-controller.js
+++ controllers/apis/restaurant-controller.js
@@ -1,44 +1,8 @@
-const { Restaurant, Category } = require('../../models')
-const { getOffset, getPagination } = require('../../helper/pagination-helper')
+const restaurantService = require('../../service/restaurant-services')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    const DEFAUT_LIMIT = 9
-    const categoryId = Number(req.query.categoryId) || ''
-    const page = Number(req.query.page) || 1
-    const limit = Number(req.query.limit) || DEFAUT_LIMIT
-    const offset = getOffset(limit, page)
-
-    return Promise.all([
-      Restaurant.findAndCountAll({
-        include: Category,
-        where: {
-          ...categoryId ? { categoryId } : {}
-        },
-        limit,
-        offset,
-        nest: true,
-        raw: true
-      }),
-      Category.findAll({ raw: true })
-    ])
-      .then(([restaurants, categories]) => {
-        const favoritedRestaurantsId = req.user?.FavoritedRestaurants ? req.user.FavoritedRestaurants.map(fr => fr.id) : []
-        const likedRestaurantsId = req.user?.FavoritedRestaurants ? req.user.LikedRestaurants.map(lr => lr.id) : []
-        const data = restaurants.rows.map(r => ({ // findAndCountAll 獲得的餐廳陣列存在 restaurants.rows
-          ...r,
-          description: r.description.substring(0, 50),
-          isFavorited: favoritedRestaurantsId.includes(r.id),
-          isLiked: likedRestaurantsId.includes(r.id)
-        }))
-        return res.json({
-          restaurants: data,
-          categories,
-          categoryId,
-          pagination: getPagination(limit, page, restaurants.count) // findAndCountAll 才能獲得 restaurants.count 搜到的資料總數
-        })
-      })
-      .catch(err => next(err))
+    restaurantService.getRestaurants(req, (err, data) => err ? next(err) : res.json(data))
   }
 }
 
---
+++ service/restaurant-services.js
@@ -0,0 +1,45 @@
+const { Restaurant, Category } = require('../models')
+const { getOffset, getPagination } = require('../helper/pagination-helper')
+
+const restaurantService = {
+  getRestaurants: (req, callback) => {
+    const DEFAUT_LIMIT = 9
+    const categoryId = Number(req.query.categoryId) || ''
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAUT_LIMIT
+    const offset = getOffset(limit, page)
+
+    return Promise.all([
+      Restaurant.findAndCountAll({
+        include: Category,
+        where: {
+          ...categoryId ? { categoryId } : {}
+        },
+        limit,
+        offset,
+        nest: true,
+        raw: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
+        const favoritedRestaurantsId = req.user?.FavoritedRestaurants ? req.user.FavoritedRestaurants.map(fr => fr.id) : []
+        const likedRestaurantsId = req.user?.FavoritedRestaurants ? req.user.LikedRestaurants.map(lr => lr.id) : []
+        const data = restaurants.rows.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50),
+          isFavorited: favoritedRestaurantsId.includes(r.id),
+          isLiked: likedRestaurantsId.includes(r.id)
+        }))
+        return callback(null, {
+          restaurants: data,
+          categories,
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.count)
+        })
+      })
+      .catch(err => callback(err))
+  }
+}
+
+module.exports = restaurantService
